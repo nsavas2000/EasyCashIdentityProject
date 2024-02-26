52de84bbc1cdc2dd69725c55c83a9282b9b2b281
N SAVAS <83330167+nsavas2000@users.noreply.github.com>
2024-02-26 22:49:11 +0300

    Identity library Added

---
+++ EasyCashIdentityProjectDataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=127.0.0.1,1433; Database=EasyCashDb; User Id=SA; Password=Bee992260.");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
