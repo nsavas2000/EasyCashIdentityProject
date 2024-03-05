daa3d70b605fbdc59b3ce99af2f158ee0f7f5ab0
N SAVAS <83330167+nsavas2000@users.noreply.github.com>
2024-03-05 10:34:59 +0300

    Business Layer Class Added

---
+++ EasyCashIdentityProjectBusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapın");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter girişi yapın");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Eposta alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Parola alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrarı alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("İki parala eşleşmemektedir");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir eposta adresi giriniz");
+        }
+    }
+}
